c1cd3bd6bbc4075ab5127a35cdbaba7b72d873a4
pachecora <pachecora2012@gmail.com>
2020-08-05 23:28:13 -0300

    Criado funçao que calcula a equaçao do 2 grau

---
+++ app.py
@@ -0,0 +1,2 @@
+from flask import Flask, url_for, render_template, redirect
+from equacao import equacao2grau
---
+++ equacao.py
@@ -0,0 +1,11 @@
+def equacao2grau(a:0, b:0, c:0) -> 'list':
+
+    """Recebe 3 inteiros a, b e c e de uma equaçao do 2grau
+    e calcula suas raízes"""
+
+    delta = (b * b) - (4 * a * c)
+    raizDelta = delta**(0.5)
+    x1 = (-b + raizDelta) / (2*a)
+    x2 = (-b - raizDelta) / (2 * a)
+    lista = [x1, x2]
+    return lista
